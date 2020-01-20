82f40c31123c5202c911a5654c88d851fdc0af21
teepiik <teemu.piik@gmail.com>
2020-01-20 18:02:47 +0200

    Login functionality started

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ import Game from './Components/Game'
 import Menu from './Components/Menubar'
 import {
     BrowserRouter as Router,
-    Route, Link, Redirect
+    Route, Redirect
 } from 'react-router-dom'
+import loginService from './Services/login'
 
 
 const App = () => {
     const [user, setUser] = useState(null)
     //const [points, setPoints] = useState('')
-    //const [username, setUsername] = useState('')
-    //const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-    const handleLogin = (user) => {
-        setUser(user)
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        console.log('loggin in with', username, password)
+        try {
+            const user = await loginService.login({ username, password })
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
     }
 
     return (
@@ -27,7 +45,13 @@ const App = () => {
                         <Menu />
                     </div>
                     <Route exact path = '/' render={() => <Home />} />
-                    <Route exact path = '/login' render={() => <Login handleLogin={handleLogin}/>} />
+                    <Route exact path = '/login' render={() =>
+                        <Login handleLogin={handleLogin}
+                            username={username}
+                            password={password}
+                            handleUsernameChange={handleUsernameChange}
+                            handlePasswordChange={handlePasswordChange}
+                        />} />
                     <Route path='/game' render={() => user ? <Game /> : <Redirect to='/login' />} />
                 </div>
             </Router>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
 
-const Login = ({ handleLogin }) => {
+const Login = (props) => {
     return (
         <div>
             <h2>Please log in to play the Lucky Click.</h2>
-            <Form onSubmit={handleLogin}>
+            <Form onSubmit={props.handleLogin}>
                 <Form.Group>
                     <Form.Label>Username: </Form.Label>
                     <Form.Control
                         type='text'
                         name='username'
+                        value={props.username}
+                        onChange={props.handleUsernameChange}
                     />
                     <Form.Label>Password: </Form.Label>
                     <Form.Control
                         type='password'
                         name='password'
+                        value={props.password}
+                        onChange={props.handlePasswordChange}
                     />
                     <Button variant='primary' type='submit'>
                         login
